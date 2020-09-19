9fe72c5051c8d580eb1d293f8c2d48bcb1bf21b7
baitsalem <baitsalem@gmail.com>
2020-09-19 20:33:05 +0200

    first commit

---
+++ msdn.py
@@ -0,0 +1,130 @@
+#TODO write a description for this script
+# @author B. AIT SALEM 
+# @category _NEW_
+# @keybinding 
+# @menupath 
+# @toolbar 
+
+
+DEFAULT_BROWSER='firefox'
+
+GOOGLE_SEARCH_PREFIX = "https://www.google.com/search?btnI&q="
+DUCKDUCKGO_SEARCH_PREFIX = "https://duckduckgo.com/?q=!ducky "
+SEARCH_PREFIX = GOOGLE_SEARCH_PREFIX
+
+from subprocess import Popen
+from ghidra.program.model.symbol import FlowType
+import httplib, urllib2
+
+
+
+
+def _getFirstMsdnLink(feed_content):
+        """
+        Parses the first MSDN URL from a RSS feed.
+        @param feed_content: a rss feed output
+        @type feed_content: str
+        @return: (str) the first MSDN url if existing.
+        """
+        while feed_content.find("<link>") > -1:
+            start_index = feed_content.find("<link>")
+            end_index = feed_content.find("</link>")
+            link_url = feed_content[len("<link>") + start_index:end_index]
+            #print(link_url)            
+            if "docs.microsoft.com" in link_url:
+                return link_url
+            else:
+                feed_content = feed_content[feed_content.find("</link>") + 7:]
+            return ""
+
+
+
+def download(url):
+        """
+        Start a blocking download. Will return the downloaded content when done.
+        @param url: The URL to download from.
+        @type url: str
+        @return: (str) the downloaded content.
+        """
+        # print "Downloader.download(): type of received parameter: ", type(url)
+        host = url[8:url.find("/", 8)]
+        path = url[url.find("/", 8):]
+        
+        print ( "host : {}".format(host)) 
+        print ( "path : {}".format(path)) 
+
+
+        try:
+            conn = httplib.HTTPSConnection(host)
+            conn.request("GET", path)
+            response = conn.getresponse()
+            if response.status == 200:
+                print "[+] Downloaded from: %s" % (url)
+                _data = response.read()
+            else:
+                print "[-] Download failed: %s (%s %s)" % (url, response.status, response.reason)
+                _data = "Download failed (%s %s)!" % (response.status, response.reason)
+            conn.close()
+        except Exception as exc:
+            print ("[!] Downloader.download: Exception while downloading: %s" % exc)
+            _data = None
+        return _data
+
+
+
+
+def getOnlineMsdnContent(keyword):
+        """
+        This functions downloads content from the MSDN website. Return the first valid result
+        @param keyword: the keyword to look up in MSDN
+        @type keyword: str
+        @return: (str) a waiting message if the keyword has been queried or a negative answer if
+            there are no entries in MSDN
+        """
+        feed_url = "https://social.msdn.microsoft.com/search/en-US/feed?format=RSS&query=%s" % keyword
+        feed_content = download(feed_url)
+        if not feed_content:
+            return "<p>Could not access the MSDN feed. Check your Internet connection.</p>"
+        msdn_url = _getFirstMsdnLink(feed_content)
+        
+        if msdn_url != "":
+            #print("Download : {}".format(msdn_url))
+            return msdn_url
+        else:
+            return "<p>Even MSDN can't help you on this one.</p>"
+
+
+
+def search_address(address):
+	search = None
+	f = getFunctionAt(address)
+	if f:
+		search = f.getName()
+		if search.startswith("FID_conflict:"):
+			search = search[13:]
+	return search
+
+search = None
+
+for r in getReferencesFrom(currentAddress):
+	if r.isExternalReference():
+		search = r.getLabel()
+		#search = r.getLibraryName() + " " + r.getLabel()
+                break
+	t = r.getReferenceType()
+	if t == FlowType.UNCONDITIONAL_CALL:
+		search = search_address(r.getToAddress())
+		if search:
+			break
+
+if search == None:
+	search = search_address(currentAddress)
+
+if search:
+	#search = SEARCH_PREFIX + search
+	#print "Opening: " + search
+        search = getOnlineMsdnContent(search)
+
+	Popen([DEFAULT_BROWSER, search])
+else:
+	print "FAILED: Nothing found to search."
